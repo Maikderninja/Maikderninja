0862d246dafc256ad8552359eb883267ad768a0f
Maikderninja <101070396+Maikderninja@users.noreply.github.com>
2022-11-14 21:56:26 +0100

    Create High end arsenal.lua

---
+++ High end arsenal.lua
@@ -0,0 +1,412 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local F6 = Instance.new("TextButton")
+local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+F6.Name = "F6"
+F6.Parent = ScreenGui
+F6.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+F6.BackgroundTransparency = 0.500
+F6.Position = UDim2.new(0.806384563, 0, 0.0453872904, 0)
+F6.Size = UDim2.new(0.159186631, 0, 0.285657197, 0)
+F6.Font = Enum.Font.SourceSans
+F6.Text = "Pwnerhub"
+F6.TextColor3 = Color3.fromRGB(0, 0, 0)
+F6.TextScaled = true
+F6.TextSize = 30.000
+F6.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+F6.TextStrokeTransparency = 0.000
+F6.TextWrapped = true
+F6.MouseButton1Down:Connect(function()
+game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" ,false ,game)
+end)
+
+UITextSizeConstraint.Parent = F6
+UITextSizeConstraint.MaxTextSize = 30
+
+P = game:GetService("Players").LocalPlayer
+P.CharacterAdded:Connect(function(char)
+    character = char
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local F6 = Instance.new("TextButton")
+local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+F6.Name = "F6"
+F6.Parent = ScreenGui
+F6.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+F6.BackgroundTransparency = 0.500
+F6.Position = UDim2.new(0.806384563, 0, 0.0453872904, 0)
+F6.Size = UDim2.new(0.159186631, 0, 0.285657197, 0)
+F6.Font = Enum.Font.SourceSans
+F6.Text = "Pwnerhub"
+F6.TextColor3 = Color3.fromRGB(0, 0, 0)
+F6.TextScaled = true
+F6.TextSize = 30.000
+F6.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+F6.TextStrokeTransparency = 0.000
+F6.TextWrapped = true
+F6.MouseButton1Down:Connect(function()
+game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" ,false ,game)
+end)
+
+UITextSizeConstraint.Parent = F6
+UITextSizeConstraint.MaxTextSize = 30
+end)
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Maikderninja/Maikderninja/main/Test.lua"))();
+
+local Window = Library.CreateLib("Arsenal PWNERS V2", "Sentinel")
+
+local Tab1 = Window:NewTab("Combat")
+local Tab2 = Window:NewTab("Visuals")
+local Tab3 = Window:NewTab("Gun mods")
+local Tab4 = Window:NewTab("Local player")
+local Tab5 = Window:NewTab("Misc")
+
+local Section1 = Tab1:NewSection("Combat")
+local Section2 = Tab2:NewSection("Visuals")
+local Section3 = Tab3:NewSection("Gun mods")
+local Section4 = Tab4:NewSection("Local player")
+local Section5 = Tab5:NewSection("Misc")
+
+Section1:NewToggle("Silent aim", "Silent aim toggle", function(silent)
+    if silent then
+        local CurrentCamera = workspace.CurrentCamera
+local Players = game.Players
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+function ClosestPlayer()
+    local MaxDist, Closest = math.huge
+    for I,V in pairs(Players.GetPlayers(Players)) do
+        if V == LocalPlayer then continue end
+        if V.Team == LocalPlayer then continue end
+        if not V.Character then continue end
+        local Head = V.Character.FindFirstChild(V.Character, "Head")
+        if not Head then continue end
+        local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, Head.Position)
+        if not Vis then continue end
+        local MousePos, TheirPos = Vector2.new(workspace.CurrentCamera.ViewportSize.X / 2, workspace.CurrentCamera.ViewportSize.Y / 2), Vector2.new(Pos.X, Pos.Y)
+        local Dist = (TheirPos - MousePos).Magnitude
+        if Dist < MaxDist then
+            MaxDist = Dist
+            Closest = V
+        end
+    end
+    return Closest
+end
+local MT = getrawmetatable(game)
+local OldNC = MT.__namecall
+local OldIDX = MT.__index
+setreadonly(MT, false)
+MT.__namecall = newcclosure(function(self, ...)
+    local Args, Method = {...}, getnamecallmethod()
+    if Method == "FindPartOnRayWithIgnoreList" and not checkcaller() then
+        local CP = ClosestPlayer()
+        if CP and CP.Character and CP.Character.FindFirstChild(CP.Character, "Head") then
+            Args[1] = Ray.new(CurrentCamera.CFrame.Position, (CP.Character.Head.Position - CurrentCamera.CFrame.Position).Unit * 1000)
+            return OldNC(self, unpack(Args))
+        end
+    end
+    return OldNC(self, ...)
+end)
+MT.__index = newcclosure(function(self, K)
+    if K == "Clips" then
+        return workspace.Map
+    end
+    return OldIDX(self, K)
+end)
+setreadonly(MT, true)
+    else
+        local CurrentCamera = workspace.CurrentCamera
+local Players = game.Players
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+function ClosestPlayer()
+    local MaxDist, Closest = math.huge
+    for I,V in pairs(Players.GetPlayers(Players)) do
+        if V == LocalPlayer then continue end
+        if V.Team == LocalPlayer then continue end
+        if not V.Character then continue end
+        local Head = V.Character.FindFirstChild(V.Character, "Head")
+        if not Head then continue end
+        local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, Head.Position)
+        if not Vis then continue end
+        local MousePos, TheirPos = Vector2.new(workspace.CurrentCamera.ViewportSize.X / 0, workspace.CurrentCamera.ViewportSize.Y / 0), Vector2.new(Pos.X, Pos.Y)
+        local Dist = (TheirPos - MousePos).Magnitude
+        if Dist < MaxDist then
+            MaxDist = Dist
+            Closest = V
+        end
+    end
+    return Closest
+end
+local MT = getrawmetatable(game)
+local OldNC = MT.__namecall
+local OldIDX = MT.__index
+setreadonly(MT, false)
+MT.__namecall = newcclosure(function(self, ...)
+    local Args, Method = {...}, getnamecallmethod()
+    if Method == "FindPartOnRayWithIgnoreList" and not checkcaller() then
+        local CP = ClosestPlayer()
+        if CP and CP.Character and CP.Character.FindFirstChild(CP.Character, "Head") then
+            Args[1] = Ray.new(CurrentCamera.CFrame.Position, (CP.Character.Head.Position - CurrentCamera.CFrame.Position).Unit * 1000)
+            return OldNC(self, unpack(Args))
+        end
+    end
+    return OldNC(self, ...)
+end)
+MT.__index = newcclosure(function(self, K)
+    if K == "Clips" then
+        return workspace.Map
+    end
+    return OldIDX(self, K)
+end)
+setreadonly(MT, true)
+    end
+end)
+
+Section1:NewToggle("Aimbot", "Aimbot toggle", function(aimbot)
+    local Players = game:GetService("Players")
+    local RunService = game:GetService("RunService")
+    local UserInputService = game:GetService("UserInputService")
+    local GuiService = game:GetService("GuiService")
+    local LocalPlayer = Players.LocalPlayer
+    local Mouse = LocalPlayer:GetMouse()
+    local Camera = workspace.CurrentCamera
+    local sc = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+    
+    local Down = true
+    local Inset = GuiService:GetGuiInset()
+    
+    --// Options \\--
+    getgenv().Options = {
+        Enabled = aimbot,
+        TeamCheck = true,
+        Triggerbot = true,
+        Smoothness = true,
+        AimPart = "Head",
+        FOV = 150
+    }
+    
+    --// Functions \\--
+    local gc = function()
+        local nearest = math.huge
+        local nearplr
+        for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v ~= game:GetService("Players").LocalPlayer and v.Character and v.Character:FindFirstChild(Options.AimPart) then
+                if Options.TeamCheck then
+                    if game:GetService("Players").LocalPlayer.Team ~= v.Team then
+                        local pos = Camera:WorldToScreenPoint(v.Character[Options.AimPart].Position)
+                        local diff = math.sqrt((pos.X - sc.X) ^ 2 + (pos.Y + Inset.Y - sc.Y) ^ 2)
+                        if diff < nearest and diff < Options.FOV then
+                            nearest = diff
+                            nearplr = v
+                        end
+                    end
+                else
+                    local pos = Camera:WorldToScreenPoint(v.Character[Options.AimPart].Position)
+                    local diff = math.sqrt((pos.X - sc.X) ^ 2 + (pos.Y + Inset.Y - sc.Y) ^ 2)
+                    if diff < nearest and diff < Options.FOV then
+                        nearest = diff
+                        nearplr = v
+                    end
+                end
+            end
+        end
+        return nearplr
+    end -- google chrome made this but i modified it for it to use teamcheck
+    
+    function Circle()
+        local circ = Drawing.new('Circle')
+        circ.Transparency = 1
+        circ.Thickness = 1.5
+        circ.Visible = true
+        circ.Color = Color3.fromRGB(255,255,255)
+        circ.Filled = false
+        circ.NumSides = 150
+        circ.Radius = Options.FOV
+        return circ
+    end
+    curc = Circle()
+    --// Main \\--
+    UserInputService.InputBegan:Connect(function( input )
+        if input.UserInputType == Enum.UserInputType.MouseButton2 then
+            Down = true
+        end
+    end)
+    UserInputService.InputEnded:Connect(function( input )
+        if input.UserInputType == Enum.UserInputType.MouseButton2 then
+            Down = false
+        end
+    end)
+    RunService.RenderStepped:Connect(function( ... )
+        if Options.Enabled then
+            if Down then
+                if gc() ~= nil and gc().Character:FindFirstChild(Options.AimPart) then
+                    if Options.Smoothness then
+                        pcall(function( ... )
+                            local Info = TweenInfo.new(0.05,Enum.EasingStyle.Linear,Enum.EasingDirection.Out)
+                            game:GetService("TweenService"):Create(Camera,Info,{
+                                CFrame = CFrame.new(Camera.CFrame.p,gc().Character[Options.AimPart].CFrame.p)
+                            }):Play()
+                        end)
+                    else
+                        pcall(function()
+                            Camera.CFrame = CFrame.new(Camera.CFrame.p,gc().Character[Options.AimPart].CFrame.p)
+                        end)
+                    end
+                end
+            end
+            curc.Visible = false
+            curc.Position = Vector2.new(Mouse.X, Mouse.Y+Inset.Y)
+        else
+            -- do nothing except remove the fov
+            curc.Visible = false
+        end
+    end)
+end)
+
+Section2:NewButton("Chams", "Chams yes", function()
+    function CreateSG(name,parent,face)
+        local SurfaceGui = Instance.new("SurfaceGui",parent)
+        SurfaceGui.Parent = parent
+        SurfaceGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+        SurfaceGui.Face = Enum.NormalId[face]
+        SurfaceGui.LightInfluence = 0
+        SurfaceGui.ResetOnSpawn = false
+        SurfaceGui.Name = name
+        SurfaceGui.AlwaysOnTop = true
+        local Frame = Instance.new("Frame",SurfaceGui)
+        Frame.BackgroundColor3 = Color3.fromRGB(85, 170, 255)
+        Frame.Size = UDim2.new(1,0,1,0)
+        end
+        while wait(1) do
+        for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+        if v ~= game:GetService("Players").LocalPlayer and v.Character ~= nil and
+        v.Character:FindFirstChild("LowerTorso") and v.Character.LowerTorso:FindFirstChild("cham") == nil then
+        for i,v in pairs (v.Character:GetChildren()) do
+        if v:IsA("MeshPart") or v.Name == "LowerTorso" then
+        CreateSG("cham",v,"Back")
+        CreateSG("cham",v,"Front")
+        CreateSG("cham",v,"Left")
+        CreateSG("cham",v,"Right")
+        CreateSG("cham",v,"Right")
+        CreateSG("cham",v,"Top")
+        CreateSG("cham",v,"Bottom")
+        end
+        end
+        end
+        end
+        end
+end)
+
+Section2:NewLabel("Esp shit is broken rn idk  why")
+
+Section3:NewButton("Rapid fire", "Executes rapid fire", function()
+    local replicationstorage = game.ReplicatedStorage
+
+for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
+   if v.Name == "Auto" then
+       v.Value = true
+   end
+   if v.Name == "RecoilControl" then
+       v.Value = 0
+   end
+   if v.Name == "MaxSpread" then
+       v.Value = 0
+   end
+   if v.Name == "ReloadTime" then
+      v.Value = 0
+   end
+   if v.Name == "FireRate" then
+       v.Value = 0.05
+   end
+   if v.Name == "Crit" then
+       v.Value = 20
+   end
+end
+end)
+
+Section3:NewButton("Infinite ammo", "Executes infinite ammo", function()
+    while wait() do
+        game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
+        game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
+    end
+end)
+
+Section3:NewButton("RGB gun", "Rainbow gun", function()
+    local c = 1 function zigzag(X)  return math.acos(math.cos(X * math.pi)) / math.pi end game:GetService("RunService").RenderStepped:Connect(function()  if game.Workspace.Camera:FindFirstChild('Arms') then   for i,v in pairs(game.Workspace.Camera.Arms:GetDescendants()) do    if v.ClassName == 'MeshPart' then      v.Color = Color3.fromHSV(zigzag(c),1,1)     c = c + .0001    end   end  end end)
+end)
+
+Section4:NewTextBox("Walkspeed", "Changes the player's walkspeed", function(speed)
+	while true do
+        wait()
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
+        end
+end)
+
+Section4:NewButton("Infinite jump", "Fly fly haha", function()
+-- Created By Zorkel
+-- Instances:
+local DropGUI = Instance.new("ScreenGui")
+local main = Instance.new("Frame")
+local DropTool = Instance.new("TextButton")
+--Properties:
+DropGUI.Name = "Drop GUI"
+DropGUI.Parent = game.CoreGui
+DropGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+main.Name = "main"
+main.Parent = DropGUI
+main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+main.Position = UDim2.new(0.0809101239, 0, 0.203790441, 0)
+main.Size = UDim2.new(0, 100, 0, 100)
+main.Active = true
+main.Draggable = true
+DropTool.Name = "Drop Tool"
+DropTool.Parent = main
+DropTool.BackgroundColor3 = Color3.fromRGB(350, 0, 0)
+DropTool.Position = UDim2.new(0.0597826242, 0, 0.119906127, 0)
+DropTool.Size = UDim2.new(0, 80, 0, 80)
+DropTool.Font = Enum.Font.SourceSans
+DropTool.Text = "^"
+DropTool.TextColor3 = Color3.fromRGB(0, 0, 0)
+DropTool.TextScaled = true
+DropTool.TextSize = 14.000
+DropTool.TextWrapped = true
+DropTool.MouseButton1Down:Connect(function()
+local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)
+game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+end)
+end)
+
+Section5:NewKeybind("Toggle ui", "Toogle the ui lol", Enum.KeyCode.F6, function()
+    Library:ToggleUI()
+end)
+
+Section5:NewButton("Copy discord invite link", "Copies the discord invite link to clipboard (JOIN!!!!)", function()
+    setclipboard("https://discord.gg/rQEHvHFGNf")
+end)
