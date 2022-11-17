47289977da74d5a0340df4d6f5f7f9a586b1ab00
Maikderninja <101070396+Maikderninja@users.noreply.github.com>
2022-11-17 16:09:49 +0100

    Update ninja legends.lua

--- ninja legends.lua
+++ ninja legends.lua
@@ -18,12 +18,12 @@ Frame.Parent = Toggle
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BackgroundTransparency = 0.500
 Frame.Position = UDim2.new(0.846244156, 0, 0.0386473425, 0)
-Frame.Size = UDim2.new(0, 112, 0, 112)
+Frame.Size = UDim2.new(0.131455392, 0, 0.270531386, 0)
 
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.BackgroundTransparency = 1.000
-TextButton.Size = UDim2.new(0, 112, 0, 112)
+TextButton.Size = UDim2.new(1, 0, 1, 0)
 TextButton.Font = Enum.Font.SourceSans
 TextButton.Text = "Pwnerhub"
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -34,7 +34,6 @@ end)
 
 P = game:GetService("Players").LocalPlayer
 P.CharacterAdded:Connect(function(char)
-    character = char
 -- Gui to Lua
 -- Version: 3.2
 
@@ -55,12 +54,12 @@ Frame.Parent = Toggle
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BackgroundTransparency = 0.500
 Frame.Position = UDim2.new(0.846244156, 0, 0.0386473425, 0)
-Frame.Size = UDim2.new(0, 112, 0, 112)
+Frame.Size = UDim2.new(0.131455392, 0, 0.270531386, 0)
 
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.BackgroundTransparency = 1.000
-TextButton.Size = UDim2.new(0, 112, 0, 112)
+TextButton.Size = UDim2.new(1, 0, 1, 0)
 TextButton.Font = Enum.Font.SourceSans
 TextButton.Text = "Pwnerhub"
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -91,7 +90,7 @@ Section1:NewToggle("Auto swing", "Auto swing (auto farm)", function(v)
         end
         local A_1 = "swingKatana"
         local Event = game:GetService("Players").LocalPlayer.ninjaEvent
-        Event:FireServer(A_1)
+        Event:FireServer(A_1)   
         wait(0.1)
     end
 end)
