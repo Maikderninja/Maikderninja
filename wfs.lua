937dc88f92f18aca157687497856260f8e93c45e
Maikderninja <101070396+Maikderninja@users.noreply.github.com>
2022-11-17 19:17:10 +0100

    Update wfs.lua

--- wfs.lua
+++ wfs.lua
@@ -1,3 +1,37 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local Toggle = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextButton = Instance.new("TextButton")
+
+--Properties:
+
+Toggle.Name = "Toggle"
+Toggle.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Toggle.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Toggle.DisplayOrder = 999999998
+
+Frame.Parent = Toggle
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BackgroundTransparency = 0.500
+Frame.Position = UDim2.new(0.846244156, 0, 0.0386473425, 0)
+Frame.Size = UDim2.new(0.131455392, 0, 0.270531386, 0)
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.Size = UDim2.new(1, 0, 1, 0)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Pwnerhub"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextSize = 30.000
+TextButton.MouseButton1Down:Connect(function()
+	game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" ,false ,game)
+end)
+
 --------------Toggles--------------
 
 --Rebirth
@@ -348,78 +382,4 @@ end)
 
 Section6:NewButton("Copy discord invite link", "Copies the discord invite link to clipboard (JOIN!!!!)", function()
     setclipboard("https://discord.gg/rQEHvHFGNf")
-end)
-
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
-
-local ScreenGui = Instance.new("ScreenGui")
-local F6 = Instance.new("TextButton")
-local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
-
---Properties:
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-F6.Name = "F6"
-F6.Parent = ScreenGui
-F6.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-F6.BackgroundTransparency = 0.500
-F6.Position = UDim2.new(0.806384563, 0, 0.0453872904, 0)
-F6.Size = UDim2.new(0.159186631, 0, 0.285657197, 0)
-F6.Font = Enum.Font.SourceSans
-F6.Text = "Pwnerhub"
-F6.TextColor3 = Color3.fromRGB(0, 0, 0)
-F6.TextScaled = true
-F6.TextSize = 30.000
-F6.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
-F6.TextStrokeTransparency = 0.000
-F6.TextWrapped = true
-F6.MouseButton1Down:Connect(function()
-game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" ,false ,game)
-end)
-
-UITextSizeConstraint.Parent = F6
-UITextSizeConstraint.MaxTextSize = 30
-
-P = game:GetService("Players").LocalPlayer
-P.CharacterAdded:Connect(function(char)
-    character = char
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
-
-local ScreenGui = Instance.new("ScreenGui")
-local F6 = Instance.new("TextButton")
-local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
-
---Properties:
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-F6.Name = "F6"
-F6.Parent = ScreenGui
-F6.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-F6.BackgroundTransparency = 0.500
-F6.Position = UDim2.new(0.806384563, 0, 0.0453872904, 0)
-F6.Size = UDim2.new(0.159186631, 0, 0.285657197, 0)
-F6.Font = Enum.Font.SourceSans
-F6.Text = "Pwnerhub"
-F6.TextColor3 = Color3.fromRGB(0, 0, 0)
-F6.TextScaled = true
-F6.TextSize = 30.000
-F6.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
-F6.TextStrokeTransparency = 0.000
-F6.TextWrapped = true
-F6.MouseButton1Down:Connect(function()
-game:GetService("VirtualInputManager"):SendKeyEvent(true, "F6" ,false ,game)
-end)
-
-UITextSizeConstraint.Parent = F6
-UITextSizeConstraint.MaxTextSize = 30
 end)
